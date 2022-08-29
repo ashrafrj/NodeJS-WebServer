3dd7713a0de433071d0436282d71c0f50abdfdc8
ashrafrj <110466285+ashrafrj@users.noreply.github.com>
2022-08-29 09:44:57 +0530

    Removed Comment in NodeJS WebServer.js

--- NodeJS WebServer.js
+++ NodeJS WebServer.js
@@ -1,16 +1,9 @@
-// Importing modules
 const express = require('express');
 const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
 
-// Sending our index.html file as
-// response. In path.join() method
-// __dirname is the directory where
-// our app.js file is present. In
-// this case __dirname is the root
-// folder of the project.
 res.sendFile(path.join(__dirname, '/index.html'));
 });
 
